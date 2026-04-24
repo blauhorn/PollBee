1b31e7ff9309e60ae3e7a2711125c6fce9ce6e9d
blauhorn <kai.berbig@gmx.de>
2026-04-24 08:52:54 +0200

    Beispieldatei für register_config und ignorieren der lokalen register_config

--- backend/register_config.py
+++
@@ -1,8 +0,0 @@
-REGISTER_GROUPS = [
-    {"groupId": "Rhythmus", "displayName": "Rhythm"},
-    {"groupId": "Saxes", "displayName": "Saxes"},
-    {"groupId": "Trombones", "displayName": "Trombones"},
-    {"groupId": "Trumpets", "displayName": "Trumpets"},
-    {"groupId": "Vocals", "displayName": "Vocals"},
-    {"groupId": "Conductor", "displayName": "Conductor"},
-]
